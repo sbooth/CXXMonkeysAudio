988ff13fbdb146335fc86d47157d0b59210e2f26
Stephen Booth <me@sbooth.org>
2024-05-21 10:30:25 -0500

    Update MAC SDK to version 10.73 (#1)

--- Sources/MAC/include/MAC/Version.h
+++ Sources/MAC/include/MAC/Version.h
@@ -3,8 +3,8 @@
 /* build batch file */
 /*
 #ifdef APE_BATCH_FILE_VERSION
-Set _MA=1071
-Set _MAV=10.71
+Set _MA=1073
+Set _MAV=10.73
 #endif
 */
 
@@ -12,8 +12,8 @@ Set _MAV=10.71
 #define APE_VERSION_MAJOR 10
 
 /* build version number */
-#define APE_VERSION_REVISION 71
-#define APE_VERSION_REVISION_NUMBER 71
+#define APE_VERSION_REVISION 73
+#define APE_VERSION_REVISION_NUMBER 73
 
 /* library interface version, update this whenever the signature of an exported function changes */
 #define APE_INTERFACE_VERSION 10
